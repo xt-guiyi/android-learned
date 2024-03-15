06302f22d813c46ba99b91ab5edfe835b1329c83
xt <1661219752@qq.com>
2024-03-15 18:36:02 +0800

    3月15号第一次提交

--- app/src/main/java/com/example/androidlearned/MainActivity.kt
+++ app/src/main/java/com/example/androidlearned/MainActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.androidlearned.databinding.ActivityMainBinding
-
+import com.example.androidlearned.customNavgination.FragmentNavigator
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
+        // navHostFragment.childFragmentManager,防止导航失败， view does not have a NavController set错误
+        navController.navigatorProvider.addNavigator("fragment",FragmentNavigator(this,navHostFragment.childFragmentManager,navHostFragment.id))
+        navController.setGraph(R.navigation.nav_graph)
+
         binding.bottomNavView.setupWithNavController(navController)
     }
 
--- app/src/test/java/com/example/androidlearned/ExampleUnitTest.kt
+++ app/src/test/java/com/example/androidlearned/ExampleUnitTest.kt
@@ -13,5 +13,67 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+       val one = Human("张三",19,"男" )
+        println("one: $one,")
+        var sort = 1
+        sort = 2
+
+        for (i  in 1..4) {
+            println(i)
+        }
+
+        val list = listOf("item1", "item2", "item3", "item4", "item5","fragment1")
+
+        for (item in list) {
+            println(item)
+        }
+        for (index in list.indices) {
+            println(index)
+        }
+
+        var index = 0
+        while (index < list.size) {
+            println("索引:$index 值: ${list.get(index)}")
+            index++
+        }
+        var enter = "哈哈"
+        var res:String?
+        when(enter) {
+            "哈哈" -> res = "when1"
+            "hh" -> res = "when2"
+            else -> res = "when3"
+        }
+        println(res)
+
+        var set = setOf("apple", "banana", "kiwifruit")
+        when{
+            "apple" in set -> println("apple")
+            "banana" in set -> println("apple")
+            "kiwifruit" in set -> println("apple")
+        }
+
+         val newList = list.filter{
+            it.contains("item")
+        }.sortedBy {
+            it.length
+         }.map {
+            it.plus("哈哈")
+        }
+        println(newList)
     }
 }
+
+
+public class Human(val name: String, val age: Number, sex: String) {
+    val sex: String = sex
+     var identify: String
+
+    init {
+        identify = "1"
+    }
+    override fun toString(): String {
+        return "Human(name='$name', age=$age, sex='$sex')"
+    }
+
+}
+
