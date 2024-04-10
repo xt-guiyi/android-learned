e564bf44d9cff0c21bd50055ee909989ddeccf8d
xt <1661219752@qq.com>
2024-04-10 18:31:15 +0800

    2024年4月10日第一次提交

--- app/src/main/java/com/example/androidlearned/dataSource/HomeDataSource.kt
+++ app/src/main/java/com/example/androidlearned/dataSource/HomeDataSource.kt
@@ -14,6 +14,13 @@ class HomeDataSource {
             homeLayoutInfos.add(HomeLayoutInfo(6,"单选按钮控件","学习radioButton控件的各种使用方式"))
             homeLayoutInfos.add(HomeLayoutInfo(7,"多选按钮控件","学习checkBox控件的各种使用方式"))
             homeLayoutInfos.add(HomeLayoutInfo(8,"切换按钮控件","学习switch控件的各种使用方式"))
+//            homeLayoutInfos.add(HomeLayoutInfo(9,"卡片布局","学习cardView控件的各种使用方式"))
+//            homeLayoutInfos.add(HomeLayoutInfo(10,"Chips控件","学习Chips的各种使用方式"))
+//            homeLayoutInfos.add(HomeLayoutInfo(11,"Dialogs弹框","学习Dialogs的各种使用方式"))
+//            homeLayoutInfos.add(HomeLayoutInfo(12,"菜单项","学习Menus的各种使用方式"))
+//            homeLayoutInfos.add(HomeLayoutInfo(13,"导航","学习底部导航栏，导航抽屉的各种使用方式"))
+//            homeLayoutInfos.add(HomeLayoutInfo(14,"进度条","学习Progress indicators的各种使用方式"))
+//            homeLayoutInfos.add(HomeLayoutInfo(14,"sheet控件","学习sheet的各种使用方式"))
 //             for (i in 1..20) {
 //                 homeLayoutInfos.add(HomeLayoutInfo("标题${i}", "描述${i}"))
 //             }
