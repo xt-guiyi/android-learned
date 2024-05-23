94bb3bb871a8739db632309271eeb8e4095bc43f
xt-guiyi <1661219752@qq.com>
2024-05-23 23:20:18 +0800

    2024年5月23号第1次提交
    Signed-off-by: xt-guiyi <1661219752@qq.com>

--- app/src/main/java/com/example/androidlearned/ui/home/fragments/RecycleViewExample5Fragment.kt
+++ app/src/main/java/com/example/androidlearned/ui/home/fragments/RecycleViewExample5Fragment.kt
@@ -1,11 +1,14 @@
 package com.example.androidlearned.ui.home.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.os.HandlerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidlearned.R
 import com.example.androidlearned.adapters.RecycleViewExample1Adapter
@@ -13,6 +16,7 @@ import com.example.androidlearned.adapters.RecycleViewExample5Adapter
 import com.example.androidlearned.dataSource.HomeDataSource
 import com.example.androidlearned.databinding.FragmentRecycleViewExample5Binding
 import com.google.android.material.divider.MaterialDividerItemDecoration
+import com.hjq.toast.Toaster
 import com.hjq.toast.Toaster.init
 
 class RecycleViewExample5Fragment : Fragment() {
@@ -30,6 +34,7 @@ class RecycleViewExample5Fragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun init() {
         val data = HomeDataSource.loadRecycleViewListByPage(1,20)
         val adapter = RecycleViewExample5Adapter(data)
@@ -42,6 +47,16 @@ class RecycleViewExample5Fragment : Fragment() {
         divide.dividerColor = ResourcesCompat.getColor(resources,R.color.silver_2,null)
         divide.isLastItemDecorated = false
         binding.refreshAndLoadMoreList.addItemDecoration(divide)
+        // 下拉刷新
+        binding.swipeRefresh.setColorSchemeColors(requireContext().resources.getColor(R.color.bilibili_pink,null))
+        binding.swipeRefresh.setOnRefreshListener {
+            Handler(requireContext().mainLooper).postDelayed({
+                Toaster.show("刷新完成")
+                data.add(0,"新增项")
+                adapter.notifyDataSetChanged()
+                binding.swipeRefresh.isRefreshing = false
+            },2000)
+        }
 
     }
 
