674cdf0093ed6662e102710e5231bff8b4cde303
xt-guiyi <1661219752@qq.com>
2024-05-26 23:43:18 +0800

    2024年5月26号第2次提交
    Signed-off-by: xt-guiyi <1661219752@qq.com>

--- app/src/main/java/com/example/androidlearned/ui/center/fragments/CenterFragment.kt
+++ app/src/main/java/com/example/androidlearned/ui/center/fragments/CenterFragment.kt
@@ -1,19 +1,20 @@
 package com.example.androidlearned.ui.center.fragments
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.androidlearned.R
 import com.example.androidlearned.adapters.CardRecycleViewAdapter
 import com.example.androidlearned.dataSource.HomeDataSource
-import com.example.androidlearned.databinding.FragmentHomeBinding
+import com.example.androidlearned.databinding.FragmentCenterBinding
+import com.example.androidlearned.ui.center.activitys.SystemBarActivity
 
 
 class CenterFragment : Fragment() {
+    lateinit var binding: FragmentCenterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,50 +24,34 @@ class CenterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_center, container, false)
+    ): View {
+        binding = FragmentCenterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecycleView()
     }
 
     /**
      * 初始化列表
      * */
-    private fun initRecycleView(binding: FragmentHomeBinding) {
-        binding.homeRecycleList.layoutManager = LinearLayoutManager(requireContext(),
+    private fun initRecycleView() {
+        binding.centerRecycleList.layoutManager = LinearLayoutManager(requireContext(),
             LinearLayoutManager.VERTICAL,false)
-        val adapter = CardRecycleViewAdapter(HomeDataSource.loadRecycleInfoList())
+        val adapter = CardRecycleViewAdapter(HomeDataSource.loadCenterRecycleInfoList())
         adapter.setOnClickListener { layoutInfo,view ->
             Toast.makeText(requireContext(), "点击了：${layoutInfo.title}", Toast.LENGTH_SHORT).show()
-            when(layoutInfo.id) {
-                1 -> view.findNavController().navigate(R.id.action_homeFragment_to_buttonWidgetActivity)
-                2 -> view.findNavController().navigate(R.id.action_homeFragment_to_textViewWidgetActivity)
-                3 -> view.findNavController().navigate(R.id.action_homeFragment_to_imageViewWidgetActivity)
-                4 -> view.findNavController().navigate(R.id.action_homeFragment_to_editTextWidgetActivity)
-                5 -> view.findNavController().navigate(R.id.action_homeFragment_to_radioButtonWidgetActivity)
-                6 -> view.findNavController().navigate(R.id.action_homeFragment_to_checkBoxWidgetActivity)
-                7 -> view.findNavController().navigate(R.id.action_homeFragment_to_switchWidgetActivity)
-                8 -> view.findNavController().navigate(R.id.action_homeFragment_to_chipsWidgetActivity)
-                9 -> view.findNavController().navigate(R.id.action_homeFragment_to_fabWidgetActivity)
-                10 -> view.findNavController().navigate(R.id.action_homeFragment_to_navigationWidgetActivity)
-                11 -> view.findNavController().navigate(R.id.action_homeFragment_to_navigationDrawActivity)
-                12 -> view.findNavController().navigate(R.id.action_homeFragment_to_menusWidgetActivity)
-                13 -> view.findNavController().navigate(R.id.action_homeFragment_to_progressIndicatorsWidgetActivity)
-                14-> view.findNavController().navigate(R.id.action_homeFragment_to_snackbarWidgetActivity)
-                15-> view.findNavController().navigate(R.id.action_homeFragment_to_toastActivity)
-                16-> view.findNavController().navigate(R.id.action_homeFragment_to_dialogActivity)
-                17-> view.findNavController().navigate(R.id.action_homeFragment_to_bottomSheetWidgetActivity)
-                18-> view.findNavController().navigate(R.id.action_homeFragment_to_seekBarWidgetActivity)
-                19-> view.findNavController().navigate(R.id.action_homeFragment_to_badgeDrawableActivity)
-                20-> view.findNavController().navigate(R.id.action_homeFragment_to_bannerWidgetActivity)
-                21-> view.findNavController().navigate(R.id.action_homeFragment_to_tabsActivity)
-                22-> view.findNavController().navigate(R.id.action_homeFragment_to_viewPageActivity)
-                23-> view.findNavController().navigate(R.id.action_homeFragment_to_recycleViewActivity)
-                24-> view.findNavController().navigate(R.id.action_homeFragment_to_otherWidgetActivity)
-                25 -> view.findNavController().navigate(R.id.action_homeFragment_to_coordinationLayoutActivity)
-
+            val intent =  when(layoutInfo.id) {
+                1 -> Intent(requireContext(),SystemBarActivity::class.java)
+                else -> null
+            }
+            intent?.let {
+                startActivity(it)
             }
         }
-        binding.homeRecycleList.adapter = adapter
+        binding.centerRecycleList.adapter = adapter
     }
 
 
