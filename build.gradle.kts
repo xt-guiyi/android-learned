772a55c14669a0f81609742bf38f74555b421def
xt-guiyi <1661219752@qq.com>
2024-06-30 19:28:53 +0800

    2024年6月30号第3次提交
    Signed-off-by: xt-guiyi <1661219752@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.1" apply false
+    id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("androidx.navigation.safeargs") version "2.7.7" apply false // 导航插件safeargs
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" // 需要和kotlin版本一致
